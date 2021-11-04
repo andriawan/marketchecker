cf7b4d68e9782a46e72a70e04fa755f5568cbafc
Muhammad Irwan Andriawan <irwan@pmg.id>
2021-11-04 23:21:35 +0700

    init

---
+++ src/main/java/andriawan/labs/marketchecker/MarketcheckerApplication.java
@@ -0,0 +1,13 @@
+package andriawan.labs.marketchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketcheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andriawan/labs/marketchecker/MarketcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package andriawan.labs.marketchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketcheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
